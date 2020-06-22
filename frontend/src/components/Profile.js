ad4fbbc3c010adc8518cc8008cde3eaabed7423d
Jessica <yutsuc@gmail.com>
2020-06-22 12:39:57 -0700

    feat: Add display for JWT token in Profile

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,8 +1,11 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import { useAuth0 } from "../utils/react-auth0-spa";
 
 const Profile = () => {
-    const { loading, user } = useAuth0();
+    const { loading, user, getTokenSilently } = useAuth0();
+    const [token, setToken] = useState("");
+    getTokenSilently().then(res => setToken(res));
+
 
     if (loading || !user) {
         return <div>Loading...</div>;
@@ -11,10 +14,10 @@ const Profile = () => {
     return (
         <Fragment>
             <img src={user.picture} alt="Profile" />
-
             <h2>{user.name}</h2>
+            <p>{user.nickname}</p>
             <p>{user.email}</p>
-            <code>{JSON.stringify(user, null, 2)}</code>
+            <textarea style={{width: "100%", resize: "none"}}defaultValue={token} rows={5} readOnly />
         </Fragment>
     );
 };
